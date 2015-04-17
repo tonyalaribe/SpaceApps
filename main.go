925e41dc594cd7e3ad166341da88b51a8fdc7790
Anthony Alaribe <anthonyalaribe@ymail.com>
2015-04-17 16:13:24 +0000

    movement works

--- main.go
+++ main.go
@@ -29,6 +29,78 @@ func printOutput(outs []byte) {
 		fmt.Printf("==> Output: %s\n", string(outs))
 	}
 }
+
+func launch(c string) {
+	var q string
+	switch c {
+	case "browser":
+		q = "google-chrome-stable"
+	case "music":
+		q = ""
+	}
+	fmt.Println(q)
+	cmd := exec.Command("bash", "-c", q)
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		fmt.Println("There was an error")
+		fmt.Println(err)
+		printError(err)
+		// Did the command fail because of an unsuccessful exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		}
+	} else {
+		// Command was successful
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
+}
+
+func openURL(URL string) {
+	q := fmt.Sprintf("xdg-open http://%s", URL)
+	fmt.Println(q)
+	cmd := exec.Command("bash", "-c", q)
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		fmt.Println("There was an error")
+		fmt.Println(err)
+		printError(err)
+		// Did the command fail because of an unsuccessful exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		}
+	} else {
+		// Command was successful
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
+}
+
+func moveAngle(x, y string) {
+	fmt.Println(x)
+	fmt.Println(y)
+	q := fmt.Sprintf("xdotool mousemove_relative --polar  %[1]s %[2]s ", x, y)
+	fmt.Println(q)
+	cmd := exec.Command("bash", "-c", q)
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		fmt.Println("There was an error")
+		fmt.Println(err)
+		printError(err)
+		// Did the command fail because of an unsuccessful exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		}
+	} else {
+		// Command was successful
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
+}
+
 func move(x, y string) {
 	fmt.Println(x)
 	fmt.Println(y)
@@ -60,7 +132,8 @@ type JobInterface interface {
 //Job struct something
 type Job struct {
 	command string
-	param   string
+	param1  string
+	param2  string
 }
 
 //SmallJob struct ish
@@ -75,24 +148,44 @@ type InvalidJob struct{ Job }
 func (job SmallJob) run() string {
 	command := job.command
 
-	param := job.param
 	switch command {
-	case "right":
-		move(param, "0")
-	case "up":
-		move("0", " -"+param)
-	case "down":
-		move("0", param)
-	case "left":
-		move("-"+param, "0")
+	case "move":
+		switch job.param1 {
+		case "right":
+			move(job.param2, "0")
+		case "up":
+			move("0", " -"+job.param2)
+		case "down":
+			move("0", job.param2)
+		case "left":
+			move("-"+job.param2, "0")
+		}
+	case "angle":
+		/*	x, err := strconv.Atoi(job.param2)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println(job.param2)
+			fmt.Println(x)
+			for i := 1; i <= x; i++ {
+				moveAngle(job.param1, "1")
+				fmt.Println(i)
+			}
+		*/
+		moveAngle(job.param1, job.param2)
+	case "open":
+		openURL(job.param1)
+	case "launch":
+		launch(job.param1)
 	}
 
-	return "done with param = " + job.param
+	return "done with param = " + job.param1
 }
 
 func (job LargeJob) run() string {
 	time.Sleep(5 * time.Second)
-	return "Completed in 5 second with param = " + job.param
+	return "Completed in 5 second with param = " + job.param1
 }
 
 func (job InvalidJob) run() string {
@@ -107,16 +200,18 @@ func jobFactory(input string) JobInterface {
 	array := strings.Split(input, " ")
 	if len(array) >= 2 {
 		command := array[0]
-		param := array[1]
+		param1 := array[1]
+		param2 := strings.TrimSuffix(array[2], "\r")
 		switch command {
-		case "left", "right", "up", "down":
+		case "move", "angle", "open", "launch":
 			return SmallJob{Job{
-				param:   param,
+				param1:  param1,
+				param2:  param2,
 				command: command,
 			}}
 		}
 	}
-	return InvalidJob{Job{param: ""}}
+	return InvalidJob{Job{param1: ""}}
 
 }
 func requestHandler(conn net.Conn, out chan string) {
@@ -144,7 +239,22 @@ func sendData(conn net.Conn, in <-chan string) {
 }
 
 func main() {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+	fmt.Println("Your System IP address(es) is(are): ")
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+			}
+		}
+	}
+	fmt.Println("While the port is 5000")
 	psock, err := net.Listen("tcp", ":5000")
+
 	if err != nil {
 		return
 	}
@@ -152,9 +262,11 @@ func main() {
 	for {
 		conn, err := psock.Accept()
 		if err != nil {
-			return
+			fmt.Println(err.Error())
 		}
-
+		fmt.Print("conection from : ")
+		fmt.Print(conn.RemoteAddr())
+		fmt.Println(conn.LocalAddr())
 		channel := make(chan string)
 		go requestHandler(conn, channel)
 		go sendData(conn, channel)
